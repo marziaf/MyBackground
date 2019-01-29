df941da092197d20c37f2ceacf0c62c45da86511
maxpesa <massimo.pesavento@outlook.com>
2019-01-29 22:47:17 +0100

    Continued developing SCInstance
    Cleaned Receiver and Sender Utils, cleaned SCI, continued developing it

--- src/videoTransfer/Client.java
+++ src/videoTransfer/Client.java
@@ -1,88 +1,52 @@
 package videoTransfer;
 
-import videoTransfer.SendFileUtils;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
-import java.io.File;
 
 public class Client { // TODO try{}finally{}
 
 	public static void main(String[] args) throws Exception {
-		// Connect
-		Socket clientSocket = connect();
-		System.out.println("Connection estabilished successfully"); // DEBUG
-		// Get files to send
-		File backgroundImage = getFile("background");
-		File video = getFile("video");
-		// Send background image
-		SendFileUtils.send(clientSocket, backgroundImage);
-		SendFileUtils.send(clientSocket, video);
-
-	}
-
-	/**
-	 * Try connection to socket until successful
-	 * 
-	 * @param clientSocket
-	 */
-	private static Socket connect() {
+		//-----------------CONNECT---------------------
+		Scanner sc = new Scanner(System.in);
+		Socket clientSocket = null;
+		//get connection info
 		boolean isValidInput = false;
-		while (!isValidInput) {
+		while(!isValidInput) {
 			try {
-				// ask for connection parameters
-				String serverAddress = getServerAddress();
-				// try connection
-				Socket clientSocket = new Socket(serverAddress, Server.Port);
-				// TODO this doesn't throw any exception if trying to connect to
-				// non-connected hosts
+				//ask for connection parameters
+				System.out.println("Server address?");
+				String serverAddress = sc.next();
+				System.out.println("Port?");
+				int port = sc.nextInt();
+				//try connection
+				clientSocket = new Socket(serverAddress, port);
+				//TODO this doesn't throw any exception if trying to connect to
+				//non-connected hosts
 				isValidInput = true;
-				return clientSocket;
 			} catch (Exception e) {
 				System.err.println("Invalid input. Connection error");
 			}
 		}
-		return null;
-	}
-
-	/**
-	 * Ask user for server to connect to
-	 * 
-	 * @return ip
-	 */
-	private static String getServerAddress() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Server address?");
-		return sc.next();
-	}
-
-	/**
-	 * Get background or video name and return file
-	 * 
-	 * @param s - "background" or "video"
-	 * @return file
-	 */ //CHECK FOR INCOMPATIBLE TIPES! ONLY PNG AND AVI FORMATS
-	private static File getFile(String s) {
-		Scanner scanner = new Scanner(System.in);
-		boolean gotValidName = false;
-		// Ask file name until a valid file is given
-		while (!gotValidName) {
-			try {
-				// Ask user for file name
-				if (s.equals("background"))
-					System.out.println("Which image do you want to set as new background?");
-				else
-					System.out.println("Which video do you want to transform?");
-				String fileName = scanner.next();
-				// Get file and check existence
-				File file = new File(fileName);
-				gotValidName = file.isFile();
-				return file;
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-
-		}
-		return null;
+		System.out.println("Connection estabilished successfully");
+		
+		//---------------OPTION MENU------------------
+		String optionMenu = 
+				"Digit one of the following commands to continue\n"
+				+ "'lv'	:	List Videos			- list the available default input videos\n"
+				+ "'cv'	:	Choose Video		- use a default video\n"
+				+ "'sv'	:	Send Video			- send a custom video\n"
+				+ "'lb'	:	List Backgrounds	- list the available default backgrounds\n"
+				+ "'cb'	:	Choose Background	- use a default background\n"
+				+ "'sv'	:	Send Background		- send a custom background\n";
+		PrintStream outToSocket = new PrintStream(clientSocket.getOutputStream());
+		
+		//TODO: get intention from user
+		
+		//TODO: get/set video/background for elaboration from user
+		
+		//TODO: wait for evaluation
+		
 	}
 
 }
--- src/videoTransfer/Server.java
+++ src/videoTransfer/Server.java
@@ -7,73 +7,81 @@ import java.net.Socket;
 import java.util.Scanner;
 
 /**
- * This Class builds and manages a Server for the application. The Server
- * listens for connections on the desired port and, acting as a manager, creates
- * a ServerConnectionInstance for every incoming connection, which lives
- * separately from all the others.
+ * This Class builds and manages a Server for the application. The
+ * Server listens for connections on the desired port and, acting as 
+ * a manager, creates a ServerConnectionInstance for every incoming
+ * connection, which lives separately from all the others.
  */
 public class Server {
-
-	// ----------DIRECTORIES PREFERENCES---------
-
-	public static final String VideoInDir = "video_in" + File.separator;
-
-	public static final String VideoOutDir = "video_out" + File.separator;
-
-	public static final String BackgroundDir = "backgrounds" + File.separator;
-
+	
+	//----------DIRECTORIES PREFERENCES---------
+	
+	public static final String VideoInDir = "video_in";
+	
+	public static final String VideoOutDir = "video_out";
+	
+	public static final String BackgroundDir = "backgrounds";
+	
 	public static final String ScriptsDir = "matlab_scripts";
-
-	// -----------CONNECTION PREFERENCES---------
-
+	
+	
+	//-----------CONNECTION PREFERENCES---------
+	
 	public static final int Port = 40000;
-
+	
 	/**
-	 * The executable method for the class. It starts a Server, which starts
-	 * listening for connections and manages to create a ServerConnectionInstance
-	 * for each
-	 * 
+	 * The executable method for the class.
+	 * It starts a Server, which starts listening for connections
+	 * and manages to create a ServerConnectionInstance for each
 	 * @param args -not used
 	 */
 	public static void main(String[] args) {
 		try {
-
-			Scanner console = new Scanner(System.in);
-
-			ServerSocket welcomeSocket = new ServerSocket(Port);
-
-			while (true) {
-				System.out.print("Press 'c' to quit, 'n' to listen for new connections: ");
-				if (console.next().equals("c"))
-					break;
-				System.out.print("Listening for new connections @ " + Port);
-				Socket newClientSocket = welcomeSocket.accept();
-				System.out.println(" connected with " + newClientSocket.getInetAddress().getHostAddress());
-				serveNewClient(newClientSocket);
-			}
-
-			// TODO: get/send files through this socket or create many instances
-			// if we want to go multi-user
-
-			// TODO: call matlab interface MatlabBinderInstance and evaluate
-
-			// TODO: return to user relevant things (again, this should be done on
-			// separate instances in case of multi-user)
-
-			welcomeSocket.close();
-		} catch (IOException e) {
-			System.out.println("IO Error, port already in use?");
+		
+		//in case the dirs that the Server needs do not exist, create them
+//		if (!(new File("backgrounds")).exists()) ...
+//		if (!(new File("video_in")).exists()) ...
+//		if (!(new File("video_out")).exists()) ...
+		Scanner console = new Scanner(System.in);
+		
+        System.out.print("Server initalization... ");
+        System.out.print("Creating server-side socket to listen for connections \n");
+		ServerSocket welcomeSocket = new ServerSocket(Port);
+		
+		// used to identify which file belongs to whom (otherwise the various instances may go blep
+		// when saving their files)
+		int instanceCounter = 1;
+		
+		while (true) {
+			System.out.print("Press 'q' to quit, 'l' to listen for new connections: ");
+			if (console.next().equals("q")) break;
+            System.out.print("Listening for new connections @ "+ Port);
+            Socket newClientSocket = welcomeSocket.accept();
+            System.out.println(" connected with "+newClientSocket.getInetAddress().getHostAddress());
+            serveNewClient(newClientSocket, instanceCounter++);
+        }		
+		
+		
+		//TODO: get/send files through this socket or create many instances
+		//if we want to go multi-user
+		
+		//TODO: call matlab interface MatlabBinderInstance and evaluate
+		
+		//TODO: return to user relevant things (again, this should be done on
+		//separate instances in case of multi-user)
+		
+		welcomeSocket.close();
 		}
+		catch (IOException e) {System.out.println("IO Error, invalid port?");}
 	}
-
+	
 	/**
 	 * Private method to serve new connections, which live on clientSocket
-	 * 
 	 * @param clientSocket the new connection socket with the new client
 	 */
-	private static void serveNewClient(Socket clientSocket) {
-		ServerConnectionInstance newServerInstance = new ServerConnectionInstance(clientSocket);
-		Thread newServerInsanceThread = new Thread(newServerInstance);
-		newServerInsanceThread.start();
-	}
+    private static void serveNewClient(Socket clientSocket, int instanceNum) {
+        ServerConnectionInstance newServerInstance = new ServerConnectionInstance(clientSocket, instanceNum);
+        Thread newServerInsanceThread = new Thread(newServerInstance);
+        newServerInsanceThread.start();
+    }
 }
