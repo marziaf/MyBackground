a61e50a3224f5f45b36ba1379efb52d2a0dcd0a4
Marzia Favaro <maarzia.f@gmail.com>
2019-02-14 15:41:48 +0100

    Removed unusefull files

--- matlab_scripts/createMaskFromBackground.m
+++
@@ -1,6 +0,0 @@
-% this section creates the mask which represents the background portion of
-% every frame
-
-% resize new background if necessary
-
-
--- matlab_scripts/substituteBackgroundFromMask.m
+++
@@ -1,5 +0,0 @@
-% this section does the actual sostitution of the background, it is common
-% for both the algorithms
-
-
-new_frames = frames.*mask + newBackground*not(mask);
